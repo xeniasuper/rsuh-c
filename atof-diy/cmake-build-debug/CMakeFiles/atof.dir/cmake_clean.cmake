937c5047b75e53634d05f1bb92f21656b1736796
xeniasuper <ichmalegern@gmail.com>
2018-12-12 22:17:28 +0300

    First commit

---
+++ atof-diy/cmake-build-debug/CMakeFiles/atof.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/atof.dir/main.c.o"
+  "atof.pdb"
+  "atof"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/atof.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
